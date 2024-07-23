5bbcb1f98bd2c419337f5246036bb068a44575e3
Brady-Fisher <bmf1418@gmail.com>
2024-07-23 11:03:13 -0500

    iframe game

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 function playGorilla(){
-    window.Telegram.WebApp.sendData('test gorilla');
+    playGame("https://cakerilla-static.pages.dev/");
 }
 
 function playMoutai(){
-    window.Telegram.WebApp.sendData('test moutai');
+    window.Telegram.WebApp.sendData('test moutai')
 }
+
+function playGame(url){
+    const mainUI = document.getElementById('mainUI');
+    const gameFrame = document.getElementById('gameFrame');
+
+    gameFrame.src = url;
+    mainUI.style.display = 'none';
+    gameFrame.style.display = 'block';
+}
+
+function closeGame(){
+    const mainUI = document.getElementById('mainUI');
+    const gameFrame = document.getElementById('gameFrame');
+
+    mainUI.style.display = 'block';
+    gameFrame.src = null;
+    gameFrame.style.display = 'none';
+}
+
+window.addEventListener('message', function(event) {
+    // Handle the message
+    console.log('Message received from iframe:', event.data);
+    // You can add custom logic here based on the message content
+    if(event.data.command == "game_result"){
+        //closeGame();
+        window.Telegram.WebApp.sendData('Game Over!')
+    }
+});
